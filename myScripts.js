a9983ea528a4f3735d163d5f7e957c7a9c375ab9
Adivyes <64081958+Adivyes@users.noreply.github.com>
2021-11-07 09:14:49 +0200

    ontime update function was added but its not working

--- myScripts.js
+++ myScripts.js
@@ -1,33 +1,76 @@
 
 let allAudioDivs = ['futureFunk','stutterBreakbeats','BassWarwick','electricGuitar','StompySlosh','GrooveB','MazePolitics','PAS3GROOVE1','SilentStar'];
+let padsDivs = ['funkId','stutterId','bassId','guiterId','stompyId','groovId','politicId','pas3Id','starId']
+let pendingAudio = [];
 
 
-function padOnClick(param,secondParam) {
 
-    let audio = document.getElementById(param);
+// function audioDelay(currentlyAudioClickedId){
 
-    if(audio.paused == true){
+//    for (let i = 0; i < allAudioDivs.length; i++) {
+
+//     let audioDiv = document.getElementById(allAudioDivs[i]); 
+//     if(allAudioDivs[i].isPlaying===true){
+
+//     pendingAudio.push(currentlyAudioClickedId)
+
+  
+//     console.log(audioDiv.id,"paused");
+//     }
+
+    
+
+//      else if (audioDiv.paused === false){
+//         console.log(audioDiv.id, "is currently playing");//this log shows me what is currently playing
+
+        
+//       }
+      
+      
+//    }
+  
+// }
+
+// function myFunction(whoIsplaying) {
+//     // Display the current position of the audio in numbers
+//     console.log(audio.currentTime);
+            
+//                 if(audio.currentTime==audio.duratin){
+
+//                     audio.play()
+//                         break
+//                 }
+            
+// }
+
+
+ function padOnClick(audioId,padId) {
+    let audio = document.getElementById(audioId);
+    let padDivId = document.getElementById(padId).id;
+
+    console.log(audio.duration);
+    if(audio.paused === true){
         audio.loop = true;
         audio.load();
         audio.play();
-       
-        document.getElementById(secondParam).getElementsByTagName('SPAN')[0].innerHTML = `${'<span class="sound"><i class="far fa-play-circle" style="font-size: 30px"></i></span>'}`;
-    }
+        document.getElementById(padId).getElementsByTagName('SPAN')[0].innerHTML = `${'<span class="sound"><i class="far fa-play-circle" style="font-size: 30px"></i></span>'}`;
+
+}
     
-    else if(audio.paused == !true){
-        let audio = document.getElementById(param);
+    else if(audio.paused === !true){
+        console.log("isPlaying === true");
+        audio = document.getElementById(audioId);
         audio.pause();
         audio.currentTime = 0;
-        document.getElementById(secondParam).getElementsByTagName('SPAN')[0].innerHTML = `${'<span class="sound"><i class="far fa-stop-circle" style="font-size: 30px"></i></span>'}`;
+        document.getElementById(padId).getElementsByTagName('SPAN')[0].innerHTML = `${'<span class="sound"><i class="far fa-stop-circle" style="font-size: 30px"></i></span>'}`;
     }
 
-    
-  }
-
+}  
+ 
   function stopAllSounds() {
   for (let i = 0; i < allAudioDivs.length; i++) {
     let soundStopBtn = document.getElementById(allAudioDivs[i]); 
-      if (soundStopBtn.paused == !true) {
+      if (soundStopBtn.paused === !true) {
         soundStopBtn.pause();
         soundStopBtn.currentTime = 0;
         document.getElementsByClassName('sound')[i].innerHTML = `${'<i class="far fa-stop-circle" style="font-size: 30px"></i>'}`;
@@ -38,12 +81,25 @@ function padOnClick(param,secondParam) {
 
   function playAllSounds(){
     for (let i = 0; i < allAudioDivs.length; i++) {
-        let soundStopBtn = document.getElementById(allAudioDivs[i]); 
-          if (soundStopBtn.paused == true) {
-            soundStopBtn.play();
+        let soundPlayBtn = document.getElementById(allAudioDivs[i]); 
+          if (soundPlayBtn.paused === true) {
+            soundPlayBtn.play();
             document.getElementsByClassName('sound')[i].innerHTML = `${'<i class="far fa-play-circle" style="font-size: 30px"></i>'}`;
 
     
       }
       }
   }
+  
+
+//     let audioCurrentTime;
+
+
+//     audio.ontimeupdate = function() {myFunction()};
+
+//     function myFunction() {
+//     // Display the current position of the audio in numbers
+//     console.log(audio.currentTime);
+//     audioCurrentTime = currentTime
+//   }
+//     audio.ontimeupdate = function() {myFunction(audio)};
